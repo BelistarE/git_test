21cd1b51b988f1ac13e13d3f0ff3ac56f62d0efa
Isabeli <86092004+BelistarE@users.noreply.github.com>
2024-05-24 15:59:25 -0700

    added javascript test

---
+++ javascript.js
@@ -0,0 +1,26 @@
+
+let finalResult;
+
+let evenOddResult;
+
+// Add your code here
+  let number1 = 1
+  let number2 = 2
+  let number3 = 3
+  let number4 = 4
+
+  let number5 = number1 + number2;
+  let number6 = number3 - number4;
+
+
+
+  finalResult = number5 * number6;
+
+
+
+
+// Don't edit the code below here!
+
+section.innerHTML = ' ';
+const para1 = document.createElement('p');
+const finalResultCheck = finalResult === 48 ? `Yes, well done!` : `No, it is ${ finalResult }`;
