972d6071fa48295998f95c933e55f7d4aa7d8714
Isabeli <86092004+BelistarE@users.noreply.github.com>
2024-06-08 13:43:52 -0700

    added js file

--- javascript.js
+++ javascript.js
@@ -24,3 +24,11 @@ let evenOddResult;
 section.innerHTML = ' ';
 const para1 = document.createElement('p');
 const finalResultCheck = finalResult === 48 ? `Yes, well done!` : `No, it is ${ finalResult }`;
+para1.textContent = `Is the finalResult 48? ${ finalResultCheck }`;
+const para2 = document.createElement('p');
+const evenOddResultCheck = evenOddResult === 0 ? 'The final result is even!' : 'The final result is odd. Hrm.';
+para2.textContent = evenOddResultCheck;
+
+section.appendChild(para1);
+section.appendChild(para2);
+    
